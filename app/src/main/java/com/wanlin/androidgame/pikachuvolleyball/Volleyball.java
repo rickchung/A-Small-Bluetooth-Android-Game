1b7a09c77d052dc865ebca6ec6def0fefa1ec893
rickchung <rickjung88@gmail.com>
2015-06-10 13:05:56 +0800

    Added VolleyBall class

---
+++ app/src/main/java/com/wanlin/androidgame/pikachuvolleyball/Volleyball.java
@@ -0,0 +1,74 @@
+package com.wanlin.androidgame.pikachuvolleyball;
+
+/**
+ * Created by rickchung on 6/9/15.
+ */
+public class Volleyball {
+    private static final String LOG_TAG = "Volleyball";
+    private static final double GRAV_ACC = 1.0;
+    private int x;
+    private int y;
+    private int objWidth;
+    private int objHeight;
+    private double speedX;
+    private double speedY;
+
+    Volleyball(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.speedX = 0.0;
+        this.speedY = 5.0;
+    }
+
+    /**
+     * Update status
+     */
+    public void update() {
+        // Update the current position
+        x += speedX;
+        y += speedY;
+        // Update speedY with gravitational acceleration
+        speedY += GRAV_ACC;
+    }
+
+    public boolean canRebound(int x, int y, int width, int height) {
+        // Left & Right (use x and width)
+        // Up & Down (use y and height)
+
+        return false;
+    }
+
+
+    /*========== Getters And Setters ==========*/
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getSpeedX() {
+        return speedX;
+    }
+
+    public void setSpeedX(double speedX) {
+        this.speedX = speedX;
+    }
+
+    public double getSpeedY() {
+        return speedY;
+    }
+
+    public void setSpeedY(double speedY) {
+        this.speedY = speedY;
+    }
+}
